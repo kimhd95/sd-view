ce0e410655a58b44b22a16e63caa56f7c82f6b3c
eojun <eojun23@gmail.com>
2020-07-04 16:56:07 +0900

    index.js 업로드

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Restaurant from "./components/pages/Island/Restaurant/Restaurant";
+import RestaurantDetail from "./components/pages/Island/Restaurant/RestaurantDetail";
+import Accommodation from "./components/pages/Island/Accommodation/Accommodation";
+import AccommodationDetail from "./components/pages/Island/Accommodation/AccommodationDetail";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      {/* <Route exact path="/" component={Accommodation} /> */}
+      <Route exact path="/" component={AccommodationDetail} />
+      <Route path="/search/:id/accommodation" component={Accommodation} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
